72c1ad45bf26561b25ac5c3996856cf0d76fc016
Anderson-11 <cq0472032023@unab.edu.sv>
2025-02-15 15:45:43 -0600

    Guía 3 Inicio del CRUD

---
+++ Temporal/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using reactBackend.Repository;
+
+// Abstraccion de un objeto Dao
+AlumnoDao alumnoDao = new AlumnoDao();
+// Llamamos el metodo que creamos en el Dao
+var alumno = alumnoDao.SelectALL();
+// Recorremos la lista 
+foreach (var item in alumno){
+    Console.WriteLine(item.Nombre);
+}
+Console.WriteLine("\n--------------------------------------\n");
+#region SelectById
+var selectById = alumnoDao.GetById(3);
+Console.WriteLine(selectById?.Nombre);
+#endregion
